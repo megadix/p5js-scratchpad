2b6a7c4ef35a230211df94354c0ec7b5ec5d4a75
Dimitri De Franciscis <dimitri@megadix.it>
2020-03-06 19:33:54 +0100

    mandelbrot.js: experiment with offscreen buffer

--- chaos/mandelbrot.js
+++ chaos/mandelbrot.js
@@ -2,69 +2,109 @@
 // https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
 
 const s = p => {
+  let div;
+
   const SCREEN_W = 800;
   const SCREEN_H = 600;
 
-  const X_MIN = -2.5;
-  const X_MAX = 1;
+  const TARGET_FRAMERATE = 10;
 
-  const Y_MIN = -1;
-  const Y_MAX = 1;
+  const X_MIN = -2.0;
+  const X_MAX = 1.0;
+
+  const Y_MIN = -2.0;
+  const Y_MAX = 2.0;
 
   const MAX_ITER = 64;
 
-  const x_increment = (X_MAX - X_MIN) / SCREEN_W;
-  const y_increment = (Y_MAX - Y_MIN) / SCREEN_H;
-
   const palette = [];
 
-  function mandelbrot() {
-    for (let screen_x = 0; screen_x <= SCREEN_W; screen_x++) {
-      for (let screen_y = 0; screen_y <= SCREEN_H; screen_y++) {
-        let x0 = p.map(screen_x, 0, SCREEN_W, X_MIN, X_MAX);
-        let y0 = p.map(screen_y, 0, SCREEN_H, Y_MIN, Y_MAX);
+  let screen_x = 0;
+  let screen_y = 0;
 
-        let re = 0.0;
-        let im = 0.0;
-        let re_square = 0.0;
-        let im_square = 0.0;
+  let frameRate;
+  let numCalcPerLoop = 1;
+  let buffer;
 
-        let i = 0;
-
-        while (i < MAX_ITER && re_square + im_square < 4) {
-          const re1 = re_square - im_square + x0;
-          const im1 = 2 * re * im + y0;
-          re = re1;
-          im = im1;
-          re_square = re * re;
-          im_square = im * im;
-
-          i++;
-        }
-
-        p.stroke(palette[i], 100, 100);
-        p.point(screen_x, screen_y);
-      }
-    }
+  function _screenToRealX(x) {
+    return p.map(x, 0, SCREEN_W, X_MIN, X_MAX);
   }
 
-  function buildPalette() {
+  function _screenToRealY(y) {
+    return p.map(y, 0, SCREEN_H, Y_MIN, Y_MAX);
+  }
+
+  function _mandelbrot() {
+    if (screen_x > SCREEN_W) {
+      console.log('STOP');
+      p.noLoop();
+      return;
+    }
+    if (screen_y > SCREEN_H) {
+      screen_x++;
+      screen_y = 0;
+    }
+
+    let x0 = _screenToRealX(screen_x);
+    let y0 = _screenToRealY(screen_y);
+
+    let re = 0.0;
+    let im = 0.0;
+    let re_square = 0.0;
+    let im_square = 0.0;
+
+    let i = 0;
+
+    while (i < MAX_ITER && re_square + im_square < 4) {
+      const re1 = re_square - im_square + x0;
+      const im1 = 2 * re * im + y0;
+      re = re1;
+      im = im1;
+      re_square = re * re;
+      im_square = im * im;
+
+      i++;
+    }
+
+    buffer.stroke(palette[i], 100, 100);
+    buffer.point(screen_x, screen_y);
+
+    screen_y++;
+  }
+
+  function _buildPalette() {
+    p.colorMode(p.HSB, MAX_ITER + 1, 100, 100);
+    buffer.colorMode(p.HSB, MAX_ITER + 1, 100, 100);
     const color_scale = MAX_ITER / Math.log(MAX_ITER);
+
     for (let i = 0; i < MAX_ITER + 1; i++) {
       palette.push(Math.trunc(Math.log(i + 1) * color_scale));
     }
   }
 
   p.setup = () => {
+    div = p.canvas.parentElement;
     p.createCanvas(SCREEN_W, SCREEN_H);
-    p.noLoop();
-    p.colorMode(p.HSB, MAX_ITER + 1, 100, 100);
-    buildPalette();
+    buffer = p.createGraphics(div.clientWidth, div.clientHeight);
+    _buildPalette();
+    buffer.background(palette[0]);
   };
 
   p.draw = () => {
-    p.background(230);
-    mandelbrot();
+    frameRate = p.frameRate();
+    if (frameRate > TARGET_FRAMERATE) {
+      numCalcPerLoop++;
+    }
+    else if (frameRate < TARGET_FRAMERATE) {
+      numCalcPerLoop--;
+    }
+
+    for (let i = 0; i < numCalcPerLoop; i++) {
+      _mandelbrot();
+    }
+
+    p.image(buffer, 0, 0);
+    p.stroke(200);
   };
 };
 
