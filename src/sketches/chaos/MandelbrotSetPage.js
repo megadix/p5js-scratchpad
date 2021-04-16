6deaf406c933e086a6c926a51c20fc17272e3d8c
Dimitri De Franciscis <dimitri@megadix.it>
2021-04-16 13:25:25 +0200

    MandelbrotSetPage: target = _blank to wikipedia

--- src/sketches/chaos/MandelbrotSetPage.js
+++ src/sketches/chaos/MandelbrotSetPage.js
@@ -4,7 +4,7 @@ import MandelbrotSetScript from "./MandelbrotSetScript";
 export default function MandelbrotSetPage() {
   return (
     <>
-      <p>One-pass rendering of the <a href="https://en.wikipedia.org/wiki/Mandelbrot_set">Mandelbrot Set</a>.</p>
+      <p>One-pass rendering of the <a href="https://en.wikipedia.org/wiki/Mandelbrot_set" target="_blank" rel="noreferrer">Mandelbrot Set</a>.</p>
       <p>
         This time I experimented with <a href="https://p5js.org/reference/#/p5/frameRate">P5.js's <code>frameRate()</code></a>:
         each <code>draw()</code> call renders as many lines as allowed by the computer power, trying to keep a steady
