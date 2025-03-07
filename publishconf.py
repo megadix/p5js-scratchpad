1729ed962575eedc8fe4e4ed49c65942f2f5f493
Dimitri De Franciscis <dimitri@megadix.it>
2025-03-07 13:38:50 +0100

    Fix SITEURL variable

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://p5js-scratchpad.web.app'
+SITEURL = 'https://p5.megadix.it/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
